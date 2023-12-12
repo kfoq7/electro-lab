4e198021813443b90dcd8c48e481febbc4dad39a
Maza92 <maza05599@gmail.com>
2023-12-12 15:15:25 -0500

    feat(*): views update

--- src/views/Gestion.java
+++ src/views/Gestion.java
@@ -6,6 +6,7 @@ import controllers.SupplierController;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import javax.management.StringValueExp;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 import lib.utils.DateTimeUtility;
@@ -31,7 +32,7 @@ public class Gestion extends javax.swing.JPanel {
 
     public Gestion(User user) throws Exception {
         initComponents();
-
+        
         this.user = user;
 
         productController = new ProductController();
@@ -42,6 +43,7 @@ public class Gestion extends javax.swing.JPanel {
         initProductTable();
 
         loadSupplier();
+       
         loadProductTable();
     }
 
@@ -58,62 +60,67 @@ public class Gestion extends javax.swing.JPanel {
         jLabel11 = new javax.swing.JLabel();
         txtStock = new javax.swing.JTextField();
         labelStock = new javax.swing.JLabel();
-        jLabel13 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tableProduct = new javax.swing.JTable();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        txtID = new javax.swing.JTextField();
+        labelNombre1 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(950, 590));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Noto Sans", 1, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("GESTIÓN DE INVENTARIO");
+        jLabel1.setText("GESTIÓN DE DISPOSITIVOS");
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(29, 31, -1, -1));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Frame 15_1.png"))); // NOI18N
-        jLabel5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Frame 15ButtonSave.png"))); // NOI18N
+        jLabel5.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
                 jLabel5MousePressed(evt);
             }
         });
-        add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, -1, -1));
-        add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, 271, 33));
+        add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 310, -1, -1));
+        add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 271, 33));
 
         labelNombre.setText("NOMBRE");
-        add(labelNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, -1, -1));
+        add(labelNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, -1, -1));
 
         jLabel4.setText("PROVEEDOR");
-        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, -1, -1));
-        add(txtFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 130, 271, 33));
+        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 230, -1, -1));
+        add(txtFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 120, 271, 30));
 
         jLabel11.setText("FECHA DE ADQUISICIÓN");
-        add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 110, -1, -1));
-        add(txtStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 200, 271, 35));
+        add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 100, -1, -1));
+        add(txtStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 180, 271, 30));
 
         labelStock.setText("STOCK");
-        add(labelStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 180, -1, -1));
+        add(labelStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 160, -1, -1));
 
-        jLabel13.setText("BUSCAR PRODUCTO");
-        add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 260, -1, -1));
+        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 250, 270, 40));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Frame 22ButtonUpdate.png"))); // NOI18N
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 310, -1, -1));
 
         tableProduct.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
             },
             new String [] {
-                "", "Id", "Nombre"
+                "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
         jScrollPane1.setViewportView(tableProduct);
-        if (tableProduct.getColumnModel().getColumnCount() > 0) {
-            tableProduct.getColumnModel().getColumn(0).setResizable(false);
-        }
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 280, 410, 200));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 230, 270, 180));
+        add(txtID, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 271, 33));
 
-        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 270, 40));
+        labelNombre1.setText("ID");
+        add(labelNombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void jLabel5MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MousePressed
@@ -139,24 +146,38 @@ public class Gestion extends javax.swing.JPanel {
         } catch (SQLException ex) {
             ErrorHandler.formException(ex);
         }
+        getDataProductField(); 
     }//GEN-LAST:event_jLabel5MousePressed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel labelNombre;
+    private javax.swing.JLabel labelNombre1;
     private javax.swing.JLabel labelStock;
     private javax.swing.JTable tableProduct;
     private javax.swing.JTextField txtFecha;
+    private javax.swing.JTextField txtID;
     private javax.swing.JTextField txtNombre;
     private javax.swing.JTextField txtStock;
     // End of variables declaration//GEN-END:variables
 
+  
+    public Object[] getDataProductField() {
+        Object[] data = {
+            txtID.getText(),
+            txtNombre.getText(),
+            txtStock.getText(),
+        };
+        return data;
+    }
+            
+    
     private void initProductTable() {
         tableProduct.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         tableProduct.getSelectedRows();
